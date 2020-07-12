ef1d248061172cec27386dfece4b7946a4f22a8a
Tubaleviao <alvaro.prata.filho@gmail.com>
2020-07-12 16:57:22 -0400

    inital dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    TooltipModule.forRoot(),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tubalar';
+  nav = ['chat', 'player', 'shooter', 'notes', 'face', 'hibo']
 }
